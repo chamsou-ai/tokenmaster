1fb88b82361c3c860fcbc7c003b1890b931f42c6
chamsou-ai <mekahalc@gmail.com>
2024-08-26 22:15:37 +0100

    Complete the Smart contract

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -1,4 +1,72 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TokenMaster {}
+contract TokenMaster is ERC721 {
+    struct Occasion{
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+    address public owner;
+    uint256 public totalOccasion = 0 ;
+    uint256 public totalSupply = 0 ;
+    mapping (uint256 => Occasion) occasions;
+    mapping (uint256 => mapping(address => bool)) public hasBought;
+    mapping (uint256 => mapping(uint256 => address)) public seatTaken;
+    mapping (uint256 => uint256[]) seatsTaken;
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function list (
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner() {
+        totalOccasion++;
+        occasions[totalOccasion] = Occasion(
+            totalOccasion,
+            _name ,
+            _cost ,
+            _maxTickets ,
+            _maxTickets ,
+            _date ,
+            _time,
+            _location
+        );
+
+    }
+
+    function mint(uint256 _id , uint256 _seat) public payable {
+        occasions[_id].tickets-=1;
+        hasBought[_id][msg.sender] = true;
+        seatTaken[_id][_seat] = msg.sender;
+        seatsTaken[_id].push(_seat);
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+    }
+
+    function getOccasion(uint256 _id) public view returns(Occasion memory){
+        return occasions[_id];
+    }
+    function getSeatsTaken(uint256 _id) public view returns(uint256[] memory){
+        return seatsTaken[_id];
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner , "This function can be eecuted only by the owner");
+        _;
+    }
+}
